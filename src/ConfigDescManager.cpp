8330c2f614271d5c387a5ddeb259446bfba7f3b2
YunQiang Su <wzssyqa@gmail.com>
2012-02-05 22:20:14 +0800

    Imported Upstream version 0.3.0

--- src/ConfigDescManager.cpp
+++ src/ConfigDescManager.cpp
@@ -1,45 +1,64 @@
-#include "ConfigDescManager.h"
+/***************************************************************************
+ *   Copyright (C) 2011~2011 by CSSlayer                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
+ ***************************************************************************/
+
+// Fcitx
 #include <fcitx-config/fcitx-config.h>
 #include <fcitx-config/xdg.h>
+
+// self
+#include "ConfigDescManager.h"
 
 namespace Fcitx
 {
 
-    ConfigDescManager::ConfigDescManager ( QObject* parent ) :
-            QObject ( parent ), m_hash ( new QHash<QString, ConfigFileDesc*> )
-    {
+ConfigDescManager::ConfigDescManager(QObject* parent) :
+    QObject(parent), m_hash(new QHash<QString, FcitxConfigFileDesc*>)
+{
 
+}
+
+ConfigDescManager::~ConfigDescManager()
+{
+    QHash<QString, FcitxConfigFileDesc*>::iterator iter;
+
+    for (iter = m_hash->begin();
+            iter != m_hash->end();
+            iter ++) {
+        FcitxConfigFreeConfigFileDesc(iter.value());
     }
 
-    ConfigDescManager::~ConfigDescManager()
-    {
-        QHash<QString, ConfigFileDesc*>::iterator iter;
+    delete m_hash;
+}
 
-        for ( iter = m_hash->begin();
-                iter != m_hash->end();
-                iter ++ )
-        {
-            FreeConfigFileDesc ( iter.value() );
-        }
+FcitxConfigFileDesc* ConfigDescManager::GetConfigDesc(const QString& name)
+{
+    if (m_hash->count(name) <= 0) {
+        FILE* fp = FcitxXDGGetFileWithPrefix("configdesc", name.toLatin1().constData(), "r", NULL);
+        FcitxConfigFileDesc* cfdesc =  FcitxConfigParseConfigFileDescFp(fp);
 
-        delete m_hash;
-    }
+        if (cfdesc)
+            m_hash->insert(name, cfdesc);
 
-    ConfigFileDesc* ConfigDescManager::GetConfigDesc ( const QString& name )
-    {
-        if ( m_hash->count ( name ) <= 0 )
-        {
-            FILE* fp = GetXDGFileWithPrefix ( "configdesc", name.toLatin1().constData(), "r", NULL );
-            ConfigFileDesc* cfdesc =  ParseConfigFileDescFp ( fp );
-
-            if ( cfdesc )
-                m_hash->insert ( name, cfdesc );
-
-            return cfdesc;
-        }
-        else
-            return ( *m_hash ) [name];
-    }
+        return cfdesc;
+    } else
+        return (*m_hash) [name];
+}
 
 }
 
--- src/ConfigDescManager.h
+++ src/ConfigDescManager.h
@@ -1,25 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2011~2011 by CSSlayer                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
+ ***************************************************************************/
+
 #ifndef CONFIGDESCMANAGER_H
 #define CONFIGDESCMANAGER_H
 
+// Qt
 #include <QObject>
 #include <QHash>
+
+// Fcitx
 #include <fcitx-config/fcitx-config.h>
 
 namespace Fcitx
 {
 
-    class ConfigDescManager : public QObject
-    {
-        Q_OBJECT
+class ConfigDescManager : public QObject
+{
+    Q_OBJECT
 
-    public:
-        ConfigDescManager ( QObject* parent = NULL );
-        virtual ~ConfigDescManager();
-        ConfigFileDesc* GetConfigDesc ( const QString& name );
+public:
+    ConfigDescManager(QObject* parent = NULL);
+    virtual ~ConfigDescManager();
+    FcitxConfigFileDesc* GetConfigDesc(const QString& name);
 
-    private:
-        QHash<QString, ConfigFileDesc*>* m_hash;
-    };
+private:
+    QHash<QString, FcitxConfigFileDesc*>* m_hash;
+};
 
 }
 
